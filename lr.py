4b9cdb17b5d98cdd977eae62f8c782bd47177569
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2025-05-11 16:43:39 -0700

    implemented lr.py with unit tests

--- lr.py
+++ lr.py
@@ -32,29 +32,113 @@ def read_data(filename):
     return (data, varnames)
 
 
+def sigmoid_function(z):
+    return 1 / (1 + exp(-z))
+
+
+def log_loss(y, y_hat):
+    return log((1+ exp( -(y * y_hat)))) / log(2)
+
+
+def vec_norm(vec):
+    magnitude = vec_mag(vec)
+    if magnitude == 0:
+        return [0] * len(vec)
+    return vec_scale(vec, 1 / magnitude)
+
+
+def vec_scale(vec, scalar):
+    return [i * scalar for i in vec]
+
+
+def vec_add(vec_a, vec_b):
+    return [a+b for a, b in zip(vec_a, vec_b)]
+
+
+def vec_sub(vec_a, vec_b):
+    return [a-b for a, b in zip(vec_a, vec_b)]
+
+
+def vec_mag(vec):
+    return sqrt(dot(vec, vec))
+
+
+def compute_gradient_of_weights(x, w, y_hat, y, l2_reg_weight):
+    num_examples = len(x)
+    nabla = [0] * len(w)
+    for i in range(num_examples):
+        error = y_hat[i] - y[i]
+        scaled_input = vec_scale(x[i], error)
+        nabla = vec_add(nabla, scaled_input)
+
+    nabla = vec_scale(nabla, 1 / num_examples)
+    penalty = vec_scale(w, l2_reg_weight)
+    nabla = vec_add(nabla, penalty)
+    return nabla
+
+
+def compute_gradient_of_bias(y_hat, y):
+    num_examples = len(y)
+    nabla = 0
+    for i in range(num_examples):
+        error = y_hat[i] - y[i]
+        nabla += error
+    return nabla / num_examples
+
+
+def get_examples_from_data(data):
+    return [example for (example, label) in data]
+
+
+def get_labels_from_data(data):
+    return [label for (example, label) in data]
+
+
+def make_predictions(model, examples):
+    return [predict_lr(model, example) for example in examples]
+
+
 # Train a logistic regression model using batch gradient descent
 def train_lr(data, eta, l2_reg_weight):
     numvars = len(data[0][0])
     w = [0.0] * numvars
     b = 0.0
+    examples = get_examples_from_data(data)
+    labels = get_labels_from_data(data)
 
-    #
-    # YOUR CODE HERE
-    #
+    for iteration in range(MAX_ITERS):
+        model = (w, b)
+        # compute predictions
+        predictions = make_predictions(model, examples)
+        # compute gradient of weights
+        nabla_w = compute_gradient_of_weights(examples, w, predictions, labels, l2_reg_weight)
+        # compute gradient of bias
+        nabla_b = compute_gradient_of_bias(predictions, labels)
+        # update weights
+        nabla_w = vec_scale(nabla_w, eta)
+        w = vec_sub(w, nabla_w)
+        # update bias
+        b -= eta * nabla_b
+        # if gradient was small (<0.0001) then stop
+        if vec_mag(nabla_w) < 0.0001: break
 
     return (w, b)
+
+
+def dot(vec_a, vec_b):
+    sum = 0.0
+    for a, b in zip(vec_a, vec_b):
+        sum += a * b
+    return sum
 
 
 # Predict the probability of the positive label (y=+1) given the
 # attributes, x.
 def predict_lr(model, x):
     (w, b) = model
-
-    #
-    # YOUR CODE HERE
-    #
-
-    return 0.5 # This is an random probability, fix this according to your solution
+    z = dot(x, w) + b
+    return sigmoid_function(z)
+    # return 0.5 # This is a random probability, fix this according to your solution
 
 
 # Load train and test data.  Learn model.  Report accuracy.
---
+++ tests/lrtests.py
@@ -0,0 +1,148 @@
+import pytest
+from lr import \
+    (sigmoid_function,
+     log_loss,
+     train_lr,
+     read_data,
+     dot,
+     vec_scale,
+     vec_add,
+     vec_sub,
+     vec_mag,
+     vec_norm,
+     compute_gradient_of_weights,
+     predict_lr,
+     compute_gradient_of_bias,
+     get_labels_from_data,
+     get_examples_from_data)
+
+
+
+def test_sigmoid():
+
+    assert sigmoid_function(0) == 0.5
+    assert round(sigmoid_function(1), 3) == 0.731
+    assert round(sigmoid_function(-1), 3) == 0.269
+
+
+def test_log_loss():
+    pass
+
+
+
+def test_train_lr():
+    # OR function
+    data = [
+        ([0, 0], 0),
+        ([0, 1], 1),
+        ([1, 0], 1),
+        ([1, 1], 1)
+    ]
+
+    eta = 1.0
+    l2_reg_weight = 0.0
+
+    model = train_lr(data, eta, l2_reg_weight)
+
+    assert predict_lr(model, data[0][0]) < 0.50
+    assert predict_lr(model, data[1][0]) > 0.50
+    assert predict_lr(model, data[2][0]) > 0.50
+    assert predict_lr(model, data[3][0]) > 0.50
+
+    # for x, label in data:
+    #     prob = predict_lr(model, x)
+    #     print(f"x = {x}, predicted = {prob:.4f}, actual = {label}")
+    #
+    # print(model)
+
+
+def test_read_data():
+    data, varnames = read_data(r'..\spambase\spambase-train.csv')
+    examples = [example for (example, label) in data]
+    labels = [label for (example, label) in data]
+    print(data)
+
+
+def test_dot():
+    a = [1, 2, 3]
+    b = [4, 5, 6]
+    assert dot(a, b) == 32
+
+
+def test_scale_vec():
+    vec = [1, 2, 3]
+    salar = 2
+    assert vec_scale(vec, salar) == [2, 4, 6]
+
+
+def test_add_vec():
+    vec_a = [1, 2, 3]
+    vec_b = [4, 5, 6]
+    assert vec_add(vec_a, vec_b) == [5, 7, 9]
+
+
+def test_vec_sub():
+    vec_a = [1, 2, 3]
+    vec_b = [4, 5, 6]
+    assert vec_sub(vec_a, vec_b) == [-3, -3, -3]
+
+
+def test_vec_mag():
+    vec = [3, 3]
+    assert round(vec_mag(vec), 3) == 4.243
+
+
+def test_vec_norm():
+    vec = [4, 0]
+    assert vec_norm(vec) == [1, 0]
+
+
+def test_compute_gradient_of_weights():
+    x = [
+        [1,2],
+        [2,1]
+    ]
+    w = [0, 0]
+    y_hat = [0.5, 0.5]
+    y = [1, 0]
+    l2_reg_weight = 0.1
+
+    assert(compute_gradient_of_weights(x, w, y_hat, y, l2_reg_weight) == [0.25, -0.25])
+
+
+def test_predict_lr():
+    x = [
+        [1,0],
+        [2,0]
+    ]
+    w = [0, 1]
+    b = 0.0
+    model = (w,b)
+    assert predict_lr(model, x[0]) == 0.5
+    assert predict_lr(model, x[1]) == 0.5
+
+
+def test_compute_gradient_of_bias():
+    y = [1,1,0,0]
+    y_hat = [0,1,1,0]
+    assert compute_gradient_of_bias(y_hat, y) == 0
+
+    y = [1,1,0,0]
+    y_hat = [0,1,1,1]
+    assert compute_gradient_of_bias(y_hat, y) == 0.25
+
+
+def test_get_examples_from_data():
+    data = [
+        ([0, 0], 1),
+        ([1, 1], 0)
+    ]
+    assert get_examples_from_data(data) == [[0,0], [1,1]]
+
+
+def test_get_labels_from_data():
+    data = [
+        ([0, 0], 1),
+        ([1, 1], 0)
+    ]
+    assert get_labels_from_data(data) == [1, 0]
